103e5b81683e75ec29f7e65b5d6f473b9e473818
dtimchenko <dmytro.timchenko1992@gmail.com>
2023-04-20 19:52:18 +0300

    django templates added

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Welcome to socal book!<h1>')
+    return render(request, 'index.html')
