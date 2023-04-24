9ba42e00d3092d68f75874680ab19076449f1b54
dtimchenko <dmytro.timchenko1992@gmail.com>
2023-04-24 20:35:00 +0300

    login/logout added

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 from .models import Profile
 from django.shortcuts import render, redirect
 
 # Create your views here.
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -24,16 +26,37 @@ def signup(request):
                 messages.info(request, 'username already exists!')
                 return redirect('signup')
             else:
-                user = User.objects.create(username=username, email=email, password=password)
+                user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('/')
         else:
             messages.info(request, 'password not match!')
             return redirect('signup')
         
     else:
         return render(request, 'signup.html')
+    
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is None:
+            messages.info(request, 'Invalid Credentials!')
+            return redirect('signin')
+        else:
+            auth.login(request, user)
+            return redirect('/')
+
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
